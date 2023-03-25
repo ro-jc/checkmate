68a95c0cbbb7a2fce38dab0179fe79f83aec03ae
Rohan Jacob <rohan.rj120@gmail.com>
2023-03-25 18:33:20 +0530

    initial commit

---
+++ checkmate/__init__.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, redirect, url_for
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=open("secret.txt").read()
+    )
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from checkmate import db
+
+    db.init_app(app)
+
+    from checkmate import auth
+
+    app.register_blueprint(auth.bp)
+
+    from checkmate import views
+
+    app.register_blueprint(views.bp)
+
+    return app
+
+
+app = create_app()
+if __name__ == "__main__":
+    app.run()
---
+++ checkmate/views.py
@@ -0,0 +1,35 @@
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+
+from checkmate.db import get_db
+
+
+bp = Blueprint("views", __name__, url_prefix="/")
+
+
+@bp.route("/")
+def index():
+    return redirect(url_for("views.friends"))
+
+
+@bp.route("/friends")
+def friends():
+    return render_template("friends.html")
+
+
+@bp.route("/groups")
+def groups():
+    return render_template("groups.html")
+
+
+@bp.route("/starred")
+def starred():
+    return render_template("starred.html")
