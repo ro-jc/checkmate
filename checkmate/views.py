d133f13dbd3de2a348a9d7644efc2648291169e1
Rohan Jacob <rohan.rj120@gmail.com>
2023-03-27 21:47:58 +0530

    add user search backend

--- checkmate/views.py
+++ checkmate/views.py
@@ -10,7 +10,7 @@ from flask import (
 )
 
 from checkmate.db import get_db
-
+from checkmate.forms import UserSearch
 
 bp = Blueprint("views", __name__, url_prefix="/")
 
@@ -38,3 +38,46 @@ def index():
 
     else:
         return render_template("landing.html")
+
+
+@bp.route("/requests", methods=["GET", "POST"])
+def requests():
+    form = UserSearch()
+    if form.validate_on_submit():
+        db = get_db()
+        username_results = db.users.aggregate(
+            [
+                {
+                    "$project": {
+                        "_id": 0,
+                        "name": "$name",
+                        "username": "$username",
+                    }
+                },
+                {"$match": {"username": {"$regex": form.name.data}}},
+                {"$sort": {"username": 1}},
+                {"$limit": 5},
+            ]
+        )
+        username_results = [user for user in username_results]
+        name_results = db.users.aggregate(
+            [
+                {
+                    "$project": {
+                        "_id": 0,
+                        "name": "$name",
+                        "username": "$username",
+                    }
+                },
+                {"$match": {"name": {"$regex": form.name.data}}},
+                {"$sort": {"name": 1}},
+                {"$limit": 15},
+            ]
+        )
+        name_results = [user for user in name_results]
+
+        return render_template(
+            "requests.html", form=form, users=username_results + name_results
+        )
+
+    return render_template("requests.html", form=form)
