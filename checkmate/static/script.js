ad299df9ac5465dcdd36876578894797b100f874
Naveen George Joseph <naveengjoseph@gmail.com>
2023-03-28 22:03:52 +0530

    sideNav size fixed with animation

--- checkmate/static/script.js
+++ checkmate/static/script.js
@@ -24,13 +24,11 @@ function friendPageOnly() {
 }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.width = "100vw";
+    document.getElementById("main").style.marginRight = "100vw";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight = "0";
-    document.body.style.backgroundColor = "white";
 }
