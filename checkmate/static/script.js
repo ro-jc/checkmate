7d0b4775fa5b96cb6b0bb86e852059883c60d6c2
Naveen George Joseph <naveengjoseph@gmail.com>
2023-04-21 12:32:24 +0530

    set b&w theme

--- checkmate/static/script.js
+++ checkmate/static/script.js
@@ -110,48 +110,48 @@ function requestSend(button, username) {
   let icon = button.children[0];
   icon.className = "fa fa-spinner fa-spin";
 
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            button.onclick = requestCancel(this, username);
-            icon.className = 'fa-solid fa-stopwatch';
-        }
-    };
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      button.onclick = requestCancel(this, username);
+      icon.className = "fa-solid fa-stopwatch";
+    }
+  };
 
   xhttp.open("GET", "/requests/send/" + username);
   xhttp.send();
 }
 
 function requestCancel(button, username) {
-    let icon = button.children[0];
-    icon.className = "fa fa-spinner fa-spin"
+  let icon = button.children[0];
+  icon.className = "fa fa-spinner fa-spin";
 
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            button.onclick = requestSend(this, username);
-            icon.className = 'fa-solid fa-user-plus';
-        }
-    };
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      button.onclick = requestSend(this, username);
+      icon.className = "fa-solid fa-user-plus";
+    }
+  };
 
-    xhttp.open("GET", "/requests/cancel/" + username);
-    xhttp.send();
+  xhttp.open("GET", "/requests/cancel/" + username);
+  xhttp.send();
 }
 
 function requestCancel(button, username) {
-    let icon = button.children[0];
-    icon.className = "fa fa-spinner fa-spin"
+  let icon = button.children[0];
+  icon.className = "fa fa-spinner fa-spin";
 
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            button.onclick = requestSend(this, username);
-            icon.className = 'fa-solid fa-user-plus';
-        }
-    };
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      button.onclick = requestSend(this, username);
+      icon.className = "fa-solid fa-user-plus";
+    }
+  };
 
-    xhttp.open("GET", "/requests/cancel/" + username);
-    xhttp.send();
+  xhttp.open("GET", "/requests/cancel/" + username);
+  xhttp.send();
 }
 
 function requestReturn(username, action) {
